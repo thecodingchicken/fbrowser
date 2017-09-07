88158d09149b4c8be13c60e67022d4f868bfce9d
thecodingchicken <crazychickenj27@gmail.com>
2017-09-07 12:28:56 -0400

    Commented in get_args

--- get_args.py
+++ get_args.py
@@ -1,4 +1,23 @@
+"""
+get_args.py
+Author: Joshua Bowe
+License: GNU GPL
+
+get_args.py is a file in the program fbrowser.
+    Versions:
+        0.0 Copyed all get_arg* functions over from fbrowser.
+        0.1 Added help on get_args()
+        0.2 Added help on get_args2()
+"""
 def get_args(string):
+    """get_args(string)
+    input: type==str
+    output: args(list), end(string)
+    you give get_args a string such as "ls -alt exampledir"
+    it will parse the string for a dash that is after a space( ' -')
+    all args are returned in args.
+    everything else is in end, which is a type str
+    that returns ['a','l','t'], "ls exampledir""""
     args=[]
     end=''
     possible = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
@@ -23,6 +42,19 @@ def get_args(string):
             
     return args,end
 def get_args2(s):
+    """
+    get_args2(s)
+    input: s == type str
+    output: list,int
+
+    s is a string such as "hello world"
+    it then returns s.split()
+    if len(s.split()) == 1, then it append None to the list and returns
+    l,1
+    if len(s.split()) == 2, then it returns l,2
+    if len(s.split()) >2, then it returns l,len(l)
+    
+    """
     if s.find('"')!=-1 or s.find("'")!=-1:
         pass
     else:
