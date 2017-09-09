eebb8cc1577a36ca01f43809e89269f1b3b8c515
thecodingchicken <crazychickenj27@gmail.com>
2017-09-09 18:27:57 -0400

    Updated get_args2
    it does not work, but it should soon.

--- get_args.py
+++ get_args.py
@@ -8,6 +8,8 @@ get_args.py is a file in the program fbrowser.
         0.0 Copyed all get_arg* functions over from fbrowser.
         0.1 Added help on get_args()
         0.2 Added help on get_args2()
+        0.3 Added find_amt, a function to find the total amounts of char in s
+        0.4 Tried to change get_args2(), but it doth not work.  
 """
 def get_args(string):
     """get_args(string)
@@ -41,6 +43,12 @@ def get_args(string):
                 ready=False
             
     return args,end
+def find_amt(s,char):
+    total=0
+    for i in s:
+        if i==char:
+            total+=1
+    return total
 def get_args2(s):
     """
     get_args2(s)
@@ -55,9 +63,57 @@ def get_args2(s):
     if len(s.split()) >2, then it returns l,len(l)
     
     """
-    if s.find('"')!=-1 or s.find("'")!=-1:
-        pass
-    else:
+    l=[]
+    s2=s[:]
+    while (s2.find('"')!=-1 or s2.find("'")!=-1) and len(s2)!=0:
+        if s2.find('"')==0:
+            if find_amt(s2,'"')%2==1:
+                print(3)
+                print("An unequal number of '\"' exists in the string")
+                raise Exception("Unequal amount of quotes")
+            else:
+                print(4)
+                foo='"'
+                try:s2=s2[s.find('"')+1:]
+                except IndexError:pass 
+                for i in range(0,len(s2)):
+                    print(s2[i])
+                    if s2[i]=='"':
+                        foo+='"'
+                        try:s2=s2[i+1]
+                        except IndexError :pass
+                        break
+                    else:
+                        foo+=s2[i]
+                l.append(foo)
+                del foo
+        elif s2.find('"')==0:
+            if find_amt(s2,"'")%2==1:
+                print("An unequal number of \"\'\" exists in the string")
+                raise Exception("Unequal amount of quotes")
+            else:
+                foo='"'
+                try:s2=s2[s.find("'")+1:]
+                except IndexError:pass 
+                for i in range(0,len(s2)):
+                    print(s2[i])
+                    if s2[i]=="'":
+                        foo+="'"
+                        try:s2=s2[i+1]
+                        except IndexError :pass
+                        break
+                    else:
+                        foo+=s2[i]
+                l.append(foo)
+                del foo
+        else:
+            try:
+                s2=s2[1:]
+            except:
+                return l,len(l)
+        print(l)
+        
+    if True:
         l=s.split()
         if len(l)==2:
             return l,2
@@ -68,3 +124,4 @@ def get_args2(s):
         else:
 ##            print("l is longer than an array of len(2)")
             return l,len(l)
+print(get_args2('h"C:\\User stuff"1'))
