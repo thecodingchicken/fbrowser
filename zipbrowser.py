33917e62e537dc4553ba26952b65b78314b2da13
thecodingchicken <crazychickenj27@gmail.com>
2017-10-13 14:11:13 -0400

    new files
    zipbrowser can now create new archives.  If the name doesn't end with .zip, it adds that.  If it exists, it will complain.

--- zipbrowser.py
+++ zipbrowser.py
@@ -1,8 +1,20 @@
 import zipfile
 import os
 import time
-
+def endswith(string,search):
+    if search==string[0-(len(search)):]:
+        return True
+    return False
+def verifyzip(name):
+    if endswith(name,'.zip')==False:
+        print("File does not end with .zip")
+        print("Changing name...")
+        return '{0}.zip'.format(name)
+    else:
+        return Name
+    raise Exception("CAN'T GET HERE!!!")
 def zipbrowser(filen):
+    print('\n'*3)
     if os.path.exists(filen):
         path=os.path.abspath(filen)
         f_name=os.path.basename(path)
@@ -12,7 +24,7 @@ def zipbrowser(filen):
     prompt=''
     while True:
         try:
-            foo=input('zipbrowser-{}> '.format(f_name))
+            foo=input('{}> '.format(f_name))
         except:
             print("Enter exit to quit")
         else:
@@ -27,6 +39,11 @@ def zipbrowser(filen):
                 else:
                     print("Error:  file not found.")
                     continue
+                if (endswith(foo[5:].strip(),'.zip')==True):
+                    print("File ends with .zip")
+                else:
+                    print("File does not fit the '*\\.zip' wildcard.")
+                    continue
                 path=os.path.abspath(foo[5:].strip())
                 f_name=os.path.basename(path)
                 print("Filename: {0}".format(path))
@@ -36,6 +53,25 @@ def zipbrowser(filen):
                 print("The full path is not needed, so you can do things that")
                 print(" are in the current directory or related to that,")
                 print("such as ../test.zip")
-                
+            elif foo[:4]=='new ':
+                print("NEW ZIP FILE")
+                name=foo[4:].strip()
+                name=verifyzip(name)
+                if os.path.exists(name):
+                    print("File exists.")
+                    print("Delete file {0}".format(os.path.abspath(name)))
+                    print("before creating a new one.")
+                    continue
+                else:
+                    print("Creating file")
+                    try:
+                        a=zipfile.ZipFile(name,'w',8)
+                        a.close()
+                    except:
+                        print("Error creating file")
+                        del a
+                    path=os.path.abspath(name)
+                    f_name=os.path.basename(path)
+                    print("Created file")
         finally:
             print('\n\n')
